610c610893ffac0ac05713bb76c0bbee52c66ea6
IttoSanzzo <konohittos@gmail.com>
2025-11-13 10:20:52 -0300

    222.count-complete-tree-nodes.ts

---
+++ 222.count-complete-tree-nodes.ts
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=222 lang=typescript
+ *
+ * [222] Count Complete Tree Nodes
+ */
+
+// @lc code=start
+// @leetcode:omit_start
+class TreeNode {
+	val: number;
+	left: TreeNode | null;
+	right: TreeNode | null;
+	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+		this.val = val === undefined ? 0 : val;
+		this.left = left === undefined ? null : left;
+		this.right = right === undefined ? null : right;
+	}
+}
+// @leetcode:omit_end
+
+function countNodes(root: TreeNode | null): number {
+	if (root == null) return 0;
+	return 1 + countNodes(root.left) + countNodes(root.right);
+}
+// @lc code=end
