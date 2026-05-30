69b93441381660a5163cd53a8fd3bba47d9a6f29
IttoSanzzo <konohittos@gmail.com>
2026-05-29 23:01:28 -0300

    231.power-of-two.ts

---
+++ 231.power-of-two.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=231 lang=typescript
+ *
+ * [231] Power of Two
+ */
+
+// @lc code=start
+function isPowerOfTwo(n: number): boolean {
+	return Number.isInteger(Math.log2(n));
+}
+// @lc code=end
